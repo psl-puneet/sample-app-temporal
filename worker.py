88d27357106a6694fd8e651d41549c7318f8becd
Puneet Sivananda <puneet_sivananda@persistent.com>
2026-04-16 11:48:34 +0530

    temporal code changes to build

--- worker.py
+++ worker.py
@@ -6,7 +6,12 @@ from temporalio.worker import Worker
 from garnishee_workflow import *
 
 async def main():
-    client = await Client.connect(os.getenv("TEMPORAL_SERVER","temporal:7233"))
+    client = await Client.connect(
+        "ap-south-1.aws.api.temporal.io:7233",
+        namespace="puneet-development.fawhu",
+        api_key="eyJhbGciOiJFUzI1NiIsICJraWQiOiJXdnR3YUEifQ.eyJhY2NvdW50X2lkIjoiZmF3aHUiLCAiYXVkIjpbInRlbXBvcmFsLmlvIl0sICJleHAiOjE4MzkzOTE3MTMsICJpc3MiOiJ0ZW1wb3JhbC5pbyIsICJqdGkiOiJoaWpWeHJ4clc3RTd1akFJNXJ5NWREZ2JkRWNaZ09IRiIsICJrZXlfaWQiOiJoaWpWeHJ4clc3RTd1akFJNXJ5NWREZ2JkRWNaZ09IRiIsICJzdWIiOiJjODNlMjE4NjkzZWY0MDkzYTRjNTE3OWYwZTgzYWM5OSJ9.4E5UYjwf6d49jXf1hOw07o2QN7Jf8du2FXaZf2Yn8qb-RqO3HmecKOeZSS9oH4uKaWaBjsnD64CywBDKL40GDA",
+        tls=True,
+    )
     worker = Worker(
         client,
         task_queue="garnishee-task-queue",
